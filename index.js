2b00925a613142c9fb28466832ec5a41e7b7c343
prograTammz <ahmedtamer1998@gmail.com>
2019-07-23 12:43:05 +0200

    Added mongoDB conenction

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const users = require('./routes/users');
 
+require('./startup/db')();
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
