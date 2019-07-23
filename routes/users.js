89d34695c69353dc2b26f0e71a6633eafcfeab90
prograTammz <ahmedtamer1998@gmail.com>
2019-07-23 12:44:11 +0200

    added register user route.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,26 @@
+//const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User, validate} = require('../models/users');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send("Hey");
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+  
+    user = new User(_.pick(req.body, ['firstName','lastName', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+  
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'firstnName','lastName', 'email']));
   });
-
 module.exports = router; 
