ad1721d481db9e58b0cf94b948ae19f249b2c63a
prograTammz <ahmedtamer1998@gmail.com>
2019-07-24 20:41:37 +0200

    Added cookieparser to app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const winston = require('winston');
+const cookieParser = require('cookie-parser')
 const app = express();
 
 const users = require('./routes/users');
@@ -11,5 +12,6 @@ const server = app.listen(port, () => winston.info(`Listening on port ${port}...
 
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/users', users);
 module.exports = server;
