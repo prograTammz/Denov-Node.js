2ada36cf8d21400145e21b60c9c22336afa85208
prograTammz <ahmedtamer1998@gmail.com>
2019-07-24 22:44:46 +0200

    added phone number to user model

--- models/users.js
+++ models/users.js
@@ -41,6 +41,12 @@ const userSchema = new mongoose.Schema({
   mainAccount:{
     type: mongoose.Schema.Types.ObjectId
   },
+  phoneNumber:{
+    type:String,
+    required: true,
+    min: 4,
+    max: 20
+  },
   isSuperAdmin:{
     type: Boolean,
     default: false
@@ -123,7 +129,8 @@ function validateRegister(user) {
     firstName: Joi.string().min(5).max(50).required(),
     lastName: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    password: Joi.string().min(5).max(255).required(),
+    phoneNumber: Joi.string().min(4).max(12).required()
   };
 
   return Joi.validate(user, schema);
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
   
-    user = new User(_.pick(req.body, ['firstName','lastName', 'email', 'password']));
+    user = new User(_.pick(req.body, ['firstName','lastName', 'email', 'password','phoneNumber']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
@@ -20,7 +20,7 @@ router.post('/', async (req, res) => {
     const token = user.generateAuthToken();
     res.cookie('token',token,{expires: new Date(Date.now() + 60*60*48*1000), httpOnly: false, secure: false });
     res.header('x-auth-token', token);
-    res.send({message:"Registeration sucessed.",user:_.pick(user, ['_id', 'firstName','lastName', 'email'])});    
+    res.send({message:"Registeration sucessed.",user:_.pick(user, ['_id', 'firstName','lastName', 'email','phoneNumber'])});    
   });
   //getting user data by decrypting the token
   router.get('/', auth, async (req, res) => {
