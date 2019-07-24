3b8e6613337b30c9974762273e5891cf162dbd45
prograTammz <ahmedtamer1998@gmail.com>
2019-07-24 22:38:06 +0200

    added auth route

--- startup/routes.js
+++ startup/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 //the routes
 const users = require('../routes/users');
+const auth = require('../routes/auth');
 
 function Route(app){
     app.use(express.json());
     app.use(cookieParser());
     app.use('/api/users', users);
+    app.use('/api/auth',auth);
 }
 
 module.exports = Route;
