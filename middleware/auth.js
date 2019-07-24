b0cd17f01ba0ac0d939c62a9077673163c742449
prograTammz <ahmedtamer1998@gmail.com>
2019-07-24 20:42:21 +0200

    auth middleware validates cookie and auth token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,13 @@ const config = require('config');
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
+  const tokenValid = req.cookies.token;
   if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (token != tokenValid) return res.status(401).send('Access denied. Token is not valid');
 
   try {
     const decoded = jwt.verify(token, process.env.API_KEY);
-    req.user = decoded; 
+    req.user = decoded;
     next();
   }
   catch (ex) {
