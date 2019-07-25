2d6ca6742fd422c8e1baef9bf1c562b54a600ea2
prograTammz <ahmedtamer1998@gmail.com>
2019-07-25 21:55:01 +0200

    added admin auth source

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  mongoose.connect('mongodb://admin:ANAbakrah98@127.0.0.1:27017/denov',{useNewUrlParser: true})
+  mongoose.connect('mongodb://admin:ANAbakrah98@127.0.0.1:27017/denov?authSource=admin',{useNewUrlParser: true})
     .then(() => winston.info(`Connected to ${'fuckme'}...`));
 }
