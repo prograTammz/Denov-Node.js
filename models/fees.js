157816d5608ec06ace3b80f9adbe2ab3736ad9d8
prograTammz <ahmedtamer1998@gmail.com>
2019-08-02 20:14:52 +0200

    added bank fees data schema

---
+++ models/fees.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const types = ['transaction','account','eservice','investment','breaking'];
+
+const fees =mongoose.model('fees', new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    cost:{
+        type: Number,
+        required: true,
+    },
+    issueDate:{
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    type:{
+        type: String,
+        enum: types,
+        required: true
+    }
+
+
+}));
+
+function validateFees(fees) {
+    const schema = {
+        name: Joi.string().required(),
+        cost: Joi.number().required(),
+        type: Joi.string().valid(types).required()
+    };
+  
+    return Joi.validate(fees, schema);
+  }
+  
+  exports.fees = fees; 
+  exports.validate = validateFees;
