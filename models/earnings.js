81157223d22df1743766e9f80c5fa0c56cb8b14d
prograTammz <ahmedtamer1998@gmail.com>
2019-08-02 21:00:10 +0200

    added earning data schema

---
+++ models/earnings.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const earning =mongoose.model('earning', new mongoose.Schema({
+    source:{
+        type: String,
+        required: true
+    },
+    cost:{
+        type: Number,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+}));
+
+function validateEarning(earning) {
+    const schema = {
+        source: Joi.string().required(),
+        cost: Joi.number().required()
+    };
+  
+    return Joi.validate(earning, schema);
+  }
+  
+  exports.earning = earning; 
+  exports.validate = validateEarning;
