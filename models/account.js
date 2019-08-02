0b24ee183681627a7380aabc29f88754332e9e18
prograTammz <ahmedtamer1998@gmail.com>
2019-08-02 19:52:27 +0200

    updated account schema

--- models/account.js
+++ models/account.js
@@ -1,16 +1,24 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 const Account =mongoose.model('account', new mongoose.Schema({
+    isMain:{
+        type: Boolean,
+        required: true
+    },
     status:{
         type: String,
         required: true,
         default: "pending",
         enum: ["pending","created"]
     },
-    balance:{
+    principle:{
         type: Number,
         required: true,
         min:5000
+    },
+    currentBalance:{
+        type: Number,
+        required: true
     },
     planId:{
         type:mongoose.Schema.Types.ObjectId,
@@ -24,15 +32,46 @@ const Account =mongoose.model('account', new mongoose.Schema({
         type: Date,
         required: true,
         default: Date.now()
+    },
+    lastUpdated:{
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    bonus: {
+        type: Number,
+        default: 0
+    },
+    allowanceDays:{
+        type: Number,
+        required: true
+    },
+    updateDays:{
+        type:Number,
+        default: 1
+    },
+    breakFees:{
+        type:Number,
+        default: 0
+    },
+    deduceRate: {
+      type: Number,
+      default: 0  
+    },
+    interest:{
+        type: Number,
+        required: true,
     }
+
 
 
 }));
 
 function validateAccount(account) {
     const schema = {
-        balance: Joi.number().min(5000).required(),
+        principle: Joi.number().min(5000).required(),
         planId: Joi.ObjectId().required(),
+        isMain: Joi.boolean().required()
     };
   
     return Joi.validate(account, schema);
