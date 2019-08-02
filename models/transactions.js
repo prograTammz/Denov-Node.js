25e60831848cca7418016a95b161e05c2a8948cf
prograTammz <ahmedtamer1998@gmail.com>
2019-08-02 20:08:27 +0200

    added bank transaction schema

---
+++ models/transactions.js
@@ -0,0 +1,47 @@
+let Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const transaction =mongoose.model('transaction', new mongoose.Schema({
+    type:{
+        type: String,
+        required: true,
+        enum:['withdraw','deposit']
+    },
+    amount:{
+        type:Number,
+        min: 2000,
+        required: true,
+    },
+    date:{
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    status:{
+        type: String,
+        required: true,
+        enum:['pending','handled','rejected'],
+        default: 'pending'
+    },
+    denovId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    bankAccountId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+}));
+
+function validateTransaction(transaction) {
+    const schema = {
+        amount: Joi.number().min(2000).required(),
+        type: Joi.string().valid('withdraw','deposit').required(),
+        bankAccountId: Joi.objectId().required()
+    };
+  
+    return Joi.validate(transaction, schema);
+  }
+  
+  exports.transaction = transaction; 
+  exports.validate = validateTransaction;
