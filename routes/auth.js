39132d47d99f02b51cc02b8072708ef5bd1b02e5
prograTammz <ahmedtamer1998@gmail.com>
2019-08-06 12:35:02 +0200

    sends some user details in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const {User, validateLogin} = require('../models/users');
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 const bcrypt = require('bcrypt');
-
-router.post('/', async (req, res) => {
+const cors = require('../middleware/cors');
+router.post('/', cors, async (req, res) => {
     const { error } = validateLogin(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
   
@@ -12,9 +13,12 @@ router.post('/', async (req, res) => {
   
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password.');
-  
+    
     const token = user.generateAuthToken();
-    res.send({token:token,message:"Logged in successfuly."});
+    user = await _.pick(user,['isAdmin','isSuperAdmin','phoneNumber','email','firstName','lastName']);
+    res.send({user: user,token:token,message:"Logged in successfuly."});
+
+    
 });
 
 module.exports = router; 
