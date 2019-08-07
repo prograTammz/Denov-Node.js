cd9279a1a650a182b39daef262b6847cf5c29681
prograTammz <ahmedtamer1998@gmail.com>
2019-08-08 01:31:54 +0200

    loggining db pass

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,7 @@ const config = require('config');
 
 module.exports = function() {
   let dbPass = process.env.DB_PASS
+  console.log(dbPass);
   mongoose.connect(`'mongodb://admin:${dbPass}@127.0.0.1:27017/denov?authSource=admin'`,{useNewUrlParser: true})
     .then(() => winston.info(`Connected to ${'fuckme'}...`));
 }
