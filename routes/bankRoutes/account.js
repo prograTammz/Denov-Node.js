e9b7ccd57eca42755321466faa0652f0605d3deb
prograTammz <ahmedtamer1998@gmail.com>
2019-08-11 21:30:55 +0200

    added init routes to accoutn route

--- routes/bankRoutes/account.js
+++ routes/bankRoutes/account.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const isBanker = require('../../middleware/banker');
+const isAdmin = require('../../middleware/admin');
 
-router.get('/',(req,res)=>{
-    res.send("Hello test");
+router.get('/',auth,(req,res)=>{
+
 });
+router.get('/:id',(req,res)=>{
 
+});
+//admin only routes
+router.get('/all',[auth,isAdmin],(req,res)=>{
+});
+//banker task routes
+router.get('/pending',[auth,isBanker],(req,res)=>{
+
+});
+router.put('/handle/:id',[auth,isBanker],(req,res)=>{
+    
+});
+router.delete('/close/:id',[auth,isBanker],(req,res)=>{
+
+});
+//for logged user
+router.post('/',auth,(req,res)=>{
+
+});
 module.exports = router;
