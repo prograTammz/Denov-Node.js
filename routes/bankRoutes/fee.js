845f988355bec10a05a08353251365d43ff43702
prograTammz <ahmedtamer1998@gmail.com>
2019-08-11 14:02:00 +0200

    added get method of fees

--- routes/bankRoutes/fee.js
+++ routes/bankRoutes/fee.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Fees,validateFees} = require('../../models/fees');
+const {Fees,validate} = require('../../models/fees');
 const _ = require('lodash');
 
 router.get('/',(req,res)=>{
@@ -8,10 +8,21 @@ router.get('/',(req,res)=>{
 });
 
 router.post('/',(req,res)=>{
-
+    const {error} = validate(req.body);
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+    const fee = new Fees({
+        name: req.body.name,
+        cost: req.body.cost,
+        type: req.body.type
+    })
+    fee.save().then(data=>{
+        res.send(data);
+    })
 })
 router.put('/',(req,res)=>{
-    
+
 })
 
 module.exports = router;
