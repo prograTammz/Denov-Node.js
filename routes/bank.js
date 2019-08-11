e5620aa536734c205d71cf1a92f7e9e0556add70
prograTammz <ahmedtamer1998@gmail.com>
2019-08-11 13:52:51 +0200

    added init fee route

--- routes/bank.js
+++ routes/bank.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const account = require('./bankRoutes/account');
 const wiretransfer = require('./bankRoutes/wiretransfer');
 const loan = require('./bankRoutes/loan');
+const fee = require('./bankRoutes/fee');
 
 router.use('/account',account);
 router.use('/wiretransfer',wiretransfer);
 router.use('/loan',loan);
+router.use('fee',fee);
 
 module.exports = router;
---
+++ routes/bankRoutes/fee.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const {Fees,validateFees} = require('../../models/fees');
+const _ = require('lodash');
+
+router.get('/',(req,res)=>{
+
+});
+
+router.post('/',(req,res)=>{
+
+})
+router.put('/',(req,res)=>{
+    
+})
+
+module.exports = router;
+
