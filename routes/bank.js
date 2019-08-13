ec302fbda4fc1eef8a38b559a7020123cdfa4ca7
prograTammz <ahmedtamer1998@gmail.com>
2019-08-13 04:15:37 +0200

    added init transaction routes

--- routes/bank.js
+++ routes/bank.js
@@ -6,11 +6,13 @@ const wiretransfer = require('./bankRoutes/wiretransfer');
 const loan = require('./bankRoutes/loan');
 const fee = require('./bankRoutes/fee');
 const plan = require('./bankRoutes/plan');
+const transaction = require('./bankRoutes/transaction');
 
 router.use('/account',account);
 router.use('/wiretransfer',wiretransfer);
 router.use('/loan',loan);
 router.use('/fee',fee);
 router.use('/plan',plan)
+router.use('/transaction',transaction)
 
 module.exports = router;
---
+++ routes/bankRoutes/transaction.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+//user routes
+router.get('/',(req,res)=>{
+
+})
+router.put('/id',(req,res)=>{
+    
+})
+router.post('/',(req,res)=>{
+
+})
+router.delete('/',(req,res)=>{
+
+})
+//admin routes
+router.get('/all',(req,res)=>{
+
+});
+router.get('/pending',(req,res)=>{
+
+})
+router.put('/verify',(req,res)=>{
+
+})
+
+
+module.exports = route
