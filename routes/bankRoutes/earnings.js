017e7d8998358356c32d338f311eb9a69d9a7045
prograTammz <ahmedtamer1998@gmail.com>
2019-08-14 11:09:25 +0200

    added earnings route functionality

--- routes/bankRoutes/earnings.js
+++ routes/bankRoutes/earnings.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/',(res,req)=>{
-
+const {Earning,validate} = require('../../models/earnings')
+router.get('/',(req,res)=>{
+    Earning.find().sort("-date")
+    .then((earnings)=>{
+        res.send(earnings);
+    }).catch((err)=>{
+        res.status(400).send(err);
+    })
 })
 router.post('/',(req,res)=>{
-
+    const {error} = validate(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+    }
+    const earning = new Earning({
+        source: req.body.source,
+        cost: req.body.cost
+    })
+    earning.save().then(()=>{
+        res.send();
+    })
 })
 module.exports = router;
