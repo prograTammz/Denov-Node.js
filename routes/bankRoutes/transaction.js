b4d1b023c13cf1e778acce7521104f6323217e66
prograTammz <ahmedtamer1998@gmail.com>
2019-08-18 18:44:31 +0200

    Restructured the routes order and imported needed files and modules

--- routes/bankRoutes/transaction.js
+++ routes/bankRoutes/transaction.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const router = express.Router();
+//Models
+const {Transaction,validateTransaction } = require('../../models/transactions');
+const {Account,validateAccount} = require('../../models/account');
+const {Fees} = require('../../models/fees');
+const {User} = require('../../models/users');
+const {Earning} = require('../../models/earnings');
+//MiddleWares
+const auth = require('../../middleware/auth');
+const banker = require('../../middleware/banker');
+const admin = require('../../middleware/admin');
+const validateObjectId = require('../../middleware/validObjectId');
+const _ = require('lodash');
+//admin routes
+router.get('/all',(req,res)=>{
+
+});
+router.get('/pending',(req,res)=>{
+
+})
+router.put('/verify',(req,res)=>{
+
+})
 //user routes
 router.get('/',(req,res)=>{
 
@@ -13,16 +35,7 @@ router.post('/',(req,res)=>{
 router.delete('/',(req,res)=>{
 
 })
-//admin routes
-router.get('/all',(req,res)=>{
 
-});
-router.get('/pending',(req,res)=>{
-
-})
-router.put('/verify',(req,res)=>{
-
-})
 
 
 module.exports = router;
