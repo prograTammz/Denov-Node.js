d7d046ab372ecd34e2145468d2cc0d6c56ccbae9
prograTammz <ahmedtamer1998@gmail.com>
2019-08-18 19:19:51 +0200

    added transaction post

--- routes/bankRoutes/transaction.js
+++ routes/bankRoutes/transaction.js
@@ -26,11 +26,30 @@ router.put('/verify',(req,res)=>{
 router.get('/',(req,res)=>{
 
 })
-router.put('/id',(req,res)=>{
+router.put('/:id',(req,res)=>{
     
 })
-router.post('/',(req,res)=>{
-
+router.post('/',auth,(req,res)=>{
+    //checking is the provided body has an error
+    const {error} = validateTransaction(req.body);
+    if(error){
+        res.status(400).send(error);
+    }
+    //Creating a transaction object
+    const transaction = new Transaction({
+        amount: req.body.amount,
+        type: req.body.type,
+        bankAccountId: req.body.bankAccountId,
+        denovId: req.user._id
+    })
+    //saving the transaction to the DB
+    transaction.save()
+        .then(()=>{
+            res.send()
+        })
+        .catch((err)=>{
+            res.status(400).send(err);
+        })
 })
 router.delete('/',(req,res)=>{
 
