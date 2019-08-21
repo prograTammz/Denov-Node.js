4d5ab77f8bed04723f919d7c9fb896c43917c5dd
prograTammz <ahmedtamer1998@gmail.com>
2019-08-21 12:55:04 +0200

    added socket.io

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const winston = require('winston');
 const app = express();
-
-const users = require('./routes/users');
+let  server = require('http').createServer(app);
+let io = require('socket.io')(server)
 
 require('./startup/db')();
 require('./startup/routes')(app);
 
-const port = process.env.PORT || 8000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
-module.exports = server;
+const roulette = io.of('/roulette')
+roulette.on("connection", socket => {
+    require('./sockets/roulette')(socket,roulette);
+  });
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => winston.info(`Listening on port ${port}...`));
