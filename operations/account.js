67d91ec66283df645c8cfc7601e5b1dacdfdb52a
prograTammz <ahmedtamer1998@gmail.com>
2019-08-23 13:38:37 +0200

    changed chargeRoulette into reusable account operation

--- operations/account.js
+++ operations/account.js
@@ -1,13 +1,11 @@
-const {Fees} = require('../../models/fees');
-const {User} = require('../../models/users');
 const {Earning} = require('../../models/earnings');
 const {Account} = require('../../models/account');
 
-function chargeRoulette(account,amount){
+function chargeAccount(account,amount, reason){
     return Account.findOneAndUpdate({_id: account._id, isMain: true},{"$inc": {"currentBalance": -amount}})
     .then(()=>{
         const earnings = new Earning({
-            source:"CASINO:: Roulette deposit",
+            source: reason,
             cost: _.sumBy(fees,'cost')
         });
         return earnings.save();
