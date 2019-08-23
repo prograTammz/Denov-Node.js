943068d979c5ee32c240751d55036b9275a15b7d
prograTammz <ahmedtamer1998@gmail.com>
2019-08-23 10:56:46 +0200

    added hasBankAccount middleware

---
+++ middleware/hasBankAccount.js
@@ -0,0 +1,15 @@
+const {Account} = require('../../models/account');
+module.exports = function (req, res, next) {
+    Account.findOne({denovId: req.user, isMain: true})
+    .then(account=>{
+        if(account){
+            req.account = account;
+            next();
+        }
+        res.status(400).send('Denov main bank account not found');
+    })
+    .catch(err=>{
+        res.status(400).send(err);
+    })
+
+}
