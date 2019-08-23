70b2646badac0b67762a2f4f2db007f26fe72d28
prograTammz <ahmedtamer1998@gmail.com>
2019-08-23 09:43:21 +0200

    added init roulette socket implementation

--- sockets/roulette.js
+++ sockets/roulette.js
@@ -3,10 +3,10 @@ const _ = require('lodash');
 var numUsers = 0;
 
 module.exports = function(socket,roulette){
-  socket.emit("wheel", _.random(0,36))
-  socket.on("connected", data =>{
-  }),
-  socket.on("spin", data=>{
-    roulette.emit("wheel", _.random(0,36))
-  })
+  roulette.emit("spinning",{});
+  setTimeout(()=>{
+    let land = _.random(0,36)
+      console.log(land);
+      roulette.emit("wheel", land );
+  },2000)
 };
