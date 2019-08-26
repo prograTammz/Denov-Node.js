0dfcf9fa6b2412bf339e74ce7f9d25128c162308
prograTammz <ahmedtamer1998@gmail.com>
2019-08-26 19:33:38 +0200

    logic error fix in hasBankAccount middle ware

--- middleware/hasBankAccount.js
+++ middleware/hasBankAccount.js
@@ -2,11 +2,13 @@ const {Account} = require('../models/account');
 module.exports = function (req, res, next) {
     Account.findOne({denovId: req.user, isMain: true})
     .then(account=>{
-        if(account){
+        if(account && account.status == "created"){
             req.account = account;
             next();
+        }else{
+            res.status(400).send('Denov main bank account not found');
         }
-        res.status(400).send('Denov main bank account not found');
+        
     })
     .catch(err=>{
         res.status(400).send(err);
